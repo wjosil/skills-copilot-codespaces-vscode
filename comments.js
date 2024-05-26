329fd419219d42e82552ddfc812f911a1c74e050
wjosil <43150545+wjosil@users.noreply.github.com>
2024-05-26 22:37:52 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,90 +1,26 @@
 // Create web server
-const express = require('express');
-const app = express();
+// Create web server
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var comments = require('./comments.json');
+var _ = require('underscore');
+var _s = require('underscore.string');
 
-// Create a server
-const server = app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/comments', function(req, res) {
+  res.json(comments);
 });
 
-// Path: .github/comments.js
-// Create a WebSocket server
-const io = require('socket.io')(server);
-
-// Path: .github/comments.js
-// Listen for new connections
-io.on('connection', (socket) => {
-  console.log('A new user connected');
-});
-
-// Path: .github/comments.js
-// Listen for new comments
-io.on('connection', (socket) => {
-  socket.on('comment', (data) => {
-    console.log(data);
-  });
-});
-
-// Path: .github/comments.js
-// Emit new comments
-io.on('connection', (socket) => {
-  socket.on('comment', (data) => {
-    io.emit('comment', data);
-  });
-});
-
-// Path: .github/comments.js
-// Listen for new comments
-io.on('connection', (socket) => {
-  socket.on('comment', (data) => {
-    io.emit('comment', data);
-    console.log(data);
-  });
-});
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/comments.js
-// Serve the client-side code
-app.use(express.static('public'));
-
-// Path: .github/public/index.html
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Comments</title>
-  </head>
-  <body>
-    <h1>Comments</h1>
-    <ul id="comments"></ul>
-    <form id="comment-form
+app.post('/api/comments', function(req, res) {
+  var comment = {
+    id: Date.now(),}})
+    // id: Date.now(),
